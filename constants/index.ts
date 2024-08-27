f0d227a953f75baa66ccd9e5226a508cb4f765f4
Adnan Sahinovic <adnanpoviolabs@gmail.com>
2024-08-27 16:42:41 +0200

    Format constants

--- constants/index.ts
+++ constants/index.ts
@@ -1,87 +1,80 @@
 export const NavLinks = [
-    // { href: '/', key: 'Templates', text: 'Limited Templates' },
-    // { href: '/', key: 'Hire Us', text: 'Hire Us' },
-    { href: '/hire', key: 'Hire Us', text: 'Pricing' },
-    // { href: '/jobs', key: 'Jobs', text: 'Get Started' },
-    { href: '/jobs', key: 'Jobs', text: 'Privacy Policy' }
+  // { href: '/', key: 'Templates', text: 'Limited Templates' },
+  // { href: '/', key: 'Hire Us', text: 'Hire Us' },
+  { href: "/hire", key: "Hire Us", text: "Pricing" },
+  // { href: '/jobs', key: 'Jobs', text: 'Get Started' },
+  { href: "/jobs", key: "Jobs", text: "Privacy Policy" },
+];
 
-  ];
-  
-  export const categoryFilters = [
- { name: "Figma", img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg"},
- { name: "Framer", img: "https://cdn.icon-icons.com/icons2/2389/PNG/512/framer_logo_icon_145269.png"},
- { name: "Webflow", img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/webflow/webflow-original.svg"},
-//  { name: "Tailwind", img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"},
-//  { name: "React Native", img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"},
-//  { name: "React", img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"},
-//  { name: "Next.js", img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg"},
+export const categoryFilters = [
+  {
+    name: "Figma",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg",
+  },
+  {
+    name: "Framer",
+    img: "https://cdn.icon-icons.com/icons2/2389/PNG/512/framer_logo_icon_145269.png",
+  },
+  {
+    name: "Webflow",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/webflow/webflow-original.svg",
+  },
+  //  { name: "Tailwind", img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"},
+  //  { name: "React Native", img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"},
+  //  { name: "React", img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"},
+  //  { name: "Next.js", img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg"},
+];
 
-  ]
-  
-  export const footerLinks = [
-    {
-      title: 'For developers',
-      links: [
-        'Go Pro!',
-        'Explore development work',
-        'Development blog',
-        'Code podcast',
-        'Open-source projects',
-        'Refer a Friend',
-        'Code of conduct',
-      ],
-    },
-    {
-      title: 'Hire developers',
-      links: [
-        'Post a job opening',
-        'Post a freelance project',
-        'Search for developers',
-      ],
-    },
-    {
-      title: 'Design & Development',
-      links: [
-        'Build faster web and mobile apps',
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        'About',
-        'Careers',
-        'Terms of service',
-        'Privacy policy',
-
-      ],
-    },
-    {
-      title: 'Directories',
-      links: [
-        'Development jobs',
-        'Developers for hire',
-        'Freelance developers for hire',
-        'Tags',
-        'Places',
-      ],
-    },
-    {
-      title: 'Development assets',
-      links: [
-        'Code Marketplace',
-        'GitHub Marketplace',
-        'NPM Registry',
-        'Packagephobia',
-      ],
-    },
-    {
-      title: 'Hire us',
-      links: [
-        'Landing Pages',
-        'Web Apps',
-        'Mobile Apps',
-        'No-code Solutions',
-      ],
-    },
-  ];
-  
+export const footerLinks = [
+  {
+    title: "For developers",
+    links: [
+      "Go Pro!",
+      "Explore development work",
+      "Development blog",
+      "Code podcast",
+      "Open-source projects",
+      "Refer a Friend",
+      "Code of conduct",
+    ],
+  },
+  {
+    title: "Hire developers",
+    links: [
+      "Post a job opening",
+      "Post a freelance project",
+      "Search for developers",
+    ],
+  },
+  {
+    title: "Design & Development",
+    links: ["Build faster web and mobile apps"],
+  },
+  {
+    title: "Company",
+    links: ["About", "Careers", "Terms of service", "Privacy policy"],
+  },
+  {
+    title: "Directories",
+    links: [
+      "Development jobs",
+      "Developers for hire",
+      "Freelance developers for hire",
+      "Tags",
+      "Places",
+    ],
+  },
+  {
+    title: "Development assets",
+    links: [
+      "Code Marketplace",
+      "GitHub Marketplace",
+      "NPM Registry",
+      "Packagephobia",
+    ],
+  },
+  {
+    title: "Hire us",
+    links: ["Landing Pages", "Web Apps", "Mobile Apps", "No-code Solutions"],
+  },
+];
