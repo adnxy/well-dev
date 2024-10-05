2dfa773fd0299f589029535623ae9acc41254694
Adnan Sahinovic <adnanpoviolabs@gmail.com>
2024-10-05 02:18:07 +0200

    Deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
         'gray-100': '#3d3d4e',
         'black-100': '#252525',
         'primary-green': '#A7F3D0',
+        'primary-navy': '#124076',
         'gray-50': '#D9D9D9',
         'gray-200': '#F3F3F4',
       },
